bc7231c7d37c30232b5a0c728cfaa1501d746ec5
JamieSWright <120191252+JamieSWright@users.noreply.github.com>
2025-07-28 11:02:52 +0100

    Add files via upload

---
+++ zoo.test.js
@@ -0,0 +1,25 @@
+const { test, expect } = require('@playwright/test');
+
+test('Navigate to Edinburgh Zoo Tiger Cam', async ({ browser }) => {
+  // Creating an Incognito Browser Context
+  const context = await browser.newContext();
+  const page = await context.newPage();
+
+  // Go to the tiger cam page
+  await page.goto('https://www.edinburghzoo.org.uk/animals/webcams/tiger-cam');
+  // Wait for the page to load
+  await page.waitForLoadState('networkidle');
+  // If the cookie modal is visible, click Accept
+  const modal = page.locator('#udc-holder-modal');
+  if (await modal.isVisible()) {
+    const acceptButton = modal.locator('button:has-text("Accept")');
+    await acceptButton.click();
+  }
+  // Verify the URL is correct
+  await expect(page).toHaveURL('https://www.edinburghzoo.org.uk/animals/webcams/tiger-cam');
+  // Verify the page title is correct
+  await expect(page).toHaveTitle('Watch the Tigers live! | Edinburgh Zoo');
+  
+  // Close the incognito context
+  await context.close();
+});
